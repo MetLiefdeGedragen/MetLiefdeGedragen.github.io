1aff19e827c41581e1f5f1229bf702f789148feb
IneMentenPXL <Ine.Menten@student.pxl.be>
2022-05-23 20:11:09 +0200

    Bugfix

--- src/app/services/contributor.service.ts
+++ src/app/services/contributor.service.ts
@@ -10,14 +10,16 @@ export class ContributorService {
       name: 'Lore',
       pictureUrl: '../../assets/fotoLore.jpg',
       description:
-        'Ik ben Lore, derdejaarsstudente vroedkunde. Ik ben geïnteresseerd in gynaecologie en prenatale raadpleging. ' +
-        'Ik vind het fijn om een diverse jobomschrijving te hebben en daarom heb ik ook voor deze studierichting gekozen.',
+        'Ik ben Lore, derdejaarsstudente vroedkunde. ' +
+        'Ik vind het fijn om een diverse jobomschrijving te hebben en daarom heb ik ook voor deze studierichting gekozen. ' +
+        'Tijdens het schrijven van mijn bachelorproef en mijn stage op het provinciaal secretariaat bij Wit-Gele Kruis kwam ik erachter dat ik geïnteresseerd ben in wetenschappelijk onderzoek. ' +
+        'Daarom zal ik mijn opleiding vervolgen met een masteropleiding. ',
     },
     {
       name: 'Femke',
       pictureUrl: '../../assets/fotoFemke.png',
       description:
-        'Hallo ik ben Femke, derdejaarsstudente vroedkunde op de PXL. Ik doe deze studie richting met heel mijn hart. ' +
+        'Hallo ik ben Femke, derdejaarsstudente vroedkunde op de PXL. Ik doe deze studierichting met heel mijn hart. ' +
         'Door de ruime keuze op basis van werk heb ik hiervoor gekozen. Zelf sta ik eigenlijk op alle afdelingen graag: ' +
         'zowel raadpleging, materniteit, verloskamer, gynaecologie, ... Ik zou me graag nog verder specialiseren in ' +
         'neonatologie na mijn opleiding vroedkunde.',
